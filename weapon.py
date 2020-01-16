deefbf0ac1fbbeb3ee6b8eab809ab74a744cd44c
itzxswitz <59517201+itzxswitz@users.noreply.github.com>
2020-01-16 07:02:59 -0800

    Update weapon.py

--- weapon.py
+++ weapon.py
@@ -2,18 +2,43 @@ import random
 
 class Weapon:
 
-    def __init__(self, name, damage, durability):
-        self.name = name
-        self.damage = damage
-        self.durability = durability
-
-# create a weapon with rng stats
-    def create_weapon(self, name, damage, durability):
-        self.name = name
-        self.damage = damage
-        self.durability = durability
-        names = ["mace", "machete", "sword", "dagger", "hammer", "whip", "axe", "shiv", "club", "ball and chain", "knife", "bat", "spear", "pipe", "stick"]
-        self.name = random.choice(names)
-        self.damage = random.randint(1,10)
-        self.durability = random.randint(1,100)
-        print("There is a " + self.name + " on the floor that does " + str(self.damage) + " damage with a durability of " + str(self.durability))
+    #def assign_weapon(self,weapon):
+       # return Weapon.weapons(weapon)
+    name = "name"
+    damage = 1
+    durability = 100
+    weapons = {
+        "mace": {
+            name: "mace",
+            damage: random.randint(3, 4),
+            durability: random.randint(20, 80)
+        },
+        "machete":{
+            name: "machete",
+            damage: random.randint(3, 5),
+            durability: random.randint(20, 70)
+       },
+        "sword":{
+            name: "sword",
+            damage: random.randint(2, 5),
+            durability: random.randint(40, 70)
+        },
+        "dagger":{
+            name: "dagger",
+            damage: random.randint(1, 2),
+            durability: random.randint(40, 80)
+        },
+        "hammer":{
+            name: "machete",
+            damage: random.randint(2, 3),
+            durability: random.randint(50, 90)
+        },
+        "fists":{
+        name: "fists",
+        damage: .5,
+        durability: 100
+        }
+    }
+    def get_weapon(self):
+        weapon = random.choice(Weapon.weapons)
+        return weapon
